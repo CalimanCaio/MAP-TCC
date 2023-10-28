7348a6dcbaa24136dd4b80c58646151e6ef2d15a
Felipe Endlich <endlichfelipe@gmail.com>
2023-10-28 10:54:42 -0300

    su

--- server/settings.py
+++ server/settings.py
@@ -101,18 +101,18 @@ DATABASES = {
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.custom_auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.custom_auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.custom_auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.custom_auth.password_validation.NumericPasswordValidator',
-    },
+    # {
+    #     'NAME': 'django.contrib.custom_auth.password_validation.UserAttributeSimilarityValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.custom_auth.password_validation.MinimumLengthValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.custom_auth.password_validation.CommonPasswordValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.custom_auth.password_validation.NumericPasswordValidator',
+    # },
 ]
 
 
